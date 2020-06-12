2b44e2e34441a94480b25d5399dddbc6131398e1
kkj6670 <jjy7201@gmail.com>
2020-06-12 15:24:50 +0900

    Updates

---
+++ precache-manifest.ec2097f26812d0972d64ef111e159cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9775a2aa0d2aa30d1a1e31026d5d0c0b",
+    "url": "/kkj6670/movie-app-2020/index.html"
+  },
+  {
+    "revision": "65b1de024a4ca623f8ff",
+    "url": "/kkj6670/movie-app-2020/static/css/main.ddb66d80.chunk.css"
+  },
+  {
+    "revision": "6fef040e5acf8e5958e6",
+    "url": "/kkj6670/movie-app-2020/static/js/2.a2503017.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kkj6670/movie-app-2020/static/js/2.a2503017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b1de024a4ca623f8ff",
+    "url": "/kkj6670/movie-app-2020/static/js/main.85062dbe.chunk.js"
+  },
+  {
+    "revision": "28ac8d8b2a5342a42d08",
+    "url": "/kkj6670/movie-app-2020/static/js/runtime-main.a1ddeff1.js"
+  }
+]);
